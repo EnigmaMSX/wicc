8f283b076d286db3c69067139ae560af0ed61645
enigma <EnigmaMSX@gmail.com>
2022-09-30 12:02:08 +0900

    Handling color profile information

--- wicc/wicc.cpp
+++ wicc/wicc.cpp
@@ -4,13 +4,33 @@
 #include <string_view>
 #include <wincodec.h>
 #include <atlbase.h>
+#include <bit>
+#include <system_error>
 
-std::wstring guidtostring(const GUID& guid)
+class COMContext
 {
-	WCHAR str[40] {};
-	if(StringFromGUID2(guid, str, static_cast<int>(std::size(str))) == 0)
+public:
+	COMContext()
+	{
+		CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
+	}
+	~COMContext() noexcept
+	{
+		CoUninitialize();
+	}
+};
+
+std::wstring guidtostring(const GUID &guid)
+{
+	WCHAR str[40]{};
+	if (StringFromGUID2(guid, str, static_cast<int>(std::size(str))) == 0)
 		std::wcout << L"failed: StringFromGUID2\n";
 	return str;
+}
+
+std::string hresult_tostr(HRESULT hr)
+{
+	return std::system_category().message(hr);
 }
 
 bool codecinfo(CComPtr<IWICBitmapCodecInfo> info)
@@ -18,7 +38,7 @@ bool codecinfo(CComPtr<IWICBitmapCodecInfo> info)
 	UINT length;
 	WCHAR buffer[1024];
 	HRESULT hr = info->GetAuthor(static_cast<UINT>(std::size(buffer)), buffer, &length);
-	if(hr != S_OK)
+	if (hr != S_OK)
 	{
 		std::wcout << L"failed: GetAuthor\n";
 		return false;
@@ -26,7 +46,7 @@ bool codecinfo(CComPtr<IWICBitmapCodecInfo> info)
 	std::wcout << L"Author: " << std::wstring_view(buffer, length) << std::endl;
 
 	hr = info->GetFriendlyName(static_cast<UINT>(std::size(buffer)), buffer, &length);
-	if(hr != S_OK)
+	if (hr != S_OK)
 	{
 		std::wcout << L"failed: GetFriendlyName\n";
 		return false;
@@ -34,7 +54,7 @@ bool codecinfo(CComPtr<IWICBitmapCodecInfo> info)
 	std::wcout << L"Name: " << std::wstring_view(buffer, length) << std::endl;
 
 	hr = info->GetFileExtensions(static_cast<UINT>(std::size(buffer)), buffer, &length);
-	if(hr != S_OK)
+	if (hr != S_OK)
 	{
 		std::wcout << L"failed: GetFileExtensions\n";
 		return false;
@@ -42,7 +62,7 @@ bool codecinfo(CComPtr<IWICBitmapCodecInfo> info)
 	std::wcout << L"Extensions: " << std::wstring_view(buffer, length) << std::endl;
 
 	hr = info->GetVersion(static_cast<UINT>(std::size(buffer)), buffer, &length);
-	if(hr != S_OK)
+	if (hr != S_OK)
 	{
 		std::wcout << L"failed: GetVersion\n";
 		return false;
@@ -51,7 +71,7 @@ bool codecinfo(CComPtr<IWICBitmapCodecInfo> info)
 
 	CLSID clsid;
 	hr = info->GetCLSID(&clsid);
-	if(hr != S_OK)
+	if (hr != S_OK)
 	{
 		std::wcout << L"failed: GetCLSID\n";
 		return false;
@@ -63,11 +83,11 @@ bool codecinfo(CComPtr<IWICBitmapCodecInfo> info)
 	return true;
 }
 
-void enumComponets(IWICImagingFactory* factory)
+void enumComponets(IWICImagingFactory *factory)
 {
 	CComPtr<IEnumUnknown> enumPtr;
 	HRESULT hr = factory->CreateComponentEnumerator(WICDecoder | WICEncoder, WICComponentEnumerateDefault, &enumPtr);
-	if(FAILED(hr))
+	if (FAILED(hr))
 	{
 		std::wcout << L"failed: CreateComponentEnumerator\n";
 		return;
@@ -75,7 +95,7 @@ void enumComponets(IWICImagingFactory* factory)
 
 	ULONG uLong;
 	CComPtr<IUnknown> component;
-	while(enumPtr->Next(1, &component, &uLong) == S_OK)
+	while (enumPtr->Next(1, &component, &uLong) == S_OK)
 	{
 		CComQIPtr<IWICBitmapCodecInfo> info(component);
 		codecinfo(info);
@@ -87,7 +107,7 @@ bool decoderinfo(CComPtr<IWICBitmapDecoder> decoder)
 {
 	CComPtr<IWICBitmapDecoderInfo> dinfo;
 	HRESULT hr = decoder->GetDecoderInfo(&dinfo);
-	if(FAILED(hr))
+	if (FAILED(hr))
 	{
 		std::wcout << L"failed: IWICBitmapDecoder::GetDecoderInfo\n";
 		return false;
@@ -97,11 +117,76 @@ bool decoderinfo(CComPtr<IWICBitmapDecoder> decoder)
 	return codecinfo(info);
 }
 
-bool load(IWICImagingFactory* factory, CComPtr<IWICBitmapDecoder> decoder)
+uint16_t readBE16(const uint8_t *ptr, size_t &pos)
+{
+	auto result = std::byteswap(*(uint16_t *)(ptr + pos));
+	pos += 2;
+	return result;
+}
+uint32_t readBE32(const uint8_t *ptr, size_t &pos)
+{
+	auto result = std::byteswap(*(uint32_t *)(ptr + pos));
+	pos += 4;
+	return result;
+}
+
+bool parseICCProfile(const uint8_t *ptr, size_t length)
+{
+	// https://www.color.org/ICC1V42.pdf
+	if (length < 128 + 4)
+		return false;
+
+	size_t pos = 0;
+	auto headerLength = readBE32(ptr, pos);
+	if (headerLength != length)
+		return false;
+
+	pos = 128;
+	auto tagCount = readBE32(ptr, pos);
+	for (size_t i = 0; i < tagCount; i++)
+	{
+		auto sig = readBE32(ptr, pos);
+		auto offset = readBE32(ptr, pos);
+		auto length = readBE32(ptr, pos);
+		if (sig == 0x64657363) //'desc'
+		{
+			pos = offset;
+			auto sig = readBE32(ptr, pos);
+			if (sig == 0x64657363) //'desc' 7bit text
+			{
+				pos += 4; // 0
+				auto descLength = readBE32(ptr, pos);
+				std::cout << "color profile: " << reinterpret_cast<const char *>(ptr + pos);
+				return true;
+			}
+			else if (sig == 0x6D6C7563)
+			{
+				//'mluc' multiLocalizedUnicodeType
+				// support first record only
+				pos += 4; // 0
+				pos += 4; // number of names
+				pos += 4; // 0xC name record size
+				pos += 2; //言語コード：ISO 639-1
+				pos += 2; //国名コード：ISO-3166/JIS X 0304
+				auto descLength = readBE32(ptr, pos);
+				auto descOffset = readBE32(ptr, pos);
+				pos = offset + descOffset;
+				std::wstring description;
+				for (size_t j = 0; j < descLength; j += 2)
+					description.push_back(std::byteswap(readBE16(ptr, pos)));
+				std::wcout << L"color profile: " << description << std::endl;
+			}
+			break;
+		}
+	}
+	return false;
+}
+
+bool load(IWICImagingFactory *factory, CComPtr<IWICBitmapDecoder> decoder)
 {
 	UINT FrameCount = 0;
 	HRESULT hr = decoder->GetFrameCount(&FrameCount);
-	if(FAILED(hr))
+	if (FAILED(hr))
 	{
 		std::wcout << L"failed: IWICBitmapDecoder::GetFrameCount\n";
 		return false;
@@ -109,10 +194,10 @@ bool load(IWICImagingFactory* factory, CComPtr<IWICBitmapDecoder> decoder)
 
 	CComPtr<IWICBitmapFrameDecode> frame;
 
-	for(UINT i = 0; i < FrameCount; i++)
+	for (UINT i = 0; i < FrameCount; i++)
 	{
 		hr = decoder->GetFrame(i, &frame);
-		if(FAILED(hr))
+		if (FAILED(hr))
 		{
 			std::wcout << L"failed: IWICBitmapDecoder::GetFrame\n";
 			return false;
@@ -120,7 +205,7 @@ bool load(IWICImagingFactory* factory, CComPtr<IWICBitmapDecoder> decoder)
 
 		UINT width, height;
 		hr = frame->GetSize(&width, &height);
-		if(FAILED(hr))
+		if (FAILED(hr))
 		{
 			std::wcout << L"failed: IWICBitmapSource::GetSize\n";
 			return false;
@@ -131,16 +216,16 @@ bool load(IWICImagingFactory* factory, CComPtr<IWICBitmapDecoder> decoder)
 
 		CComPtr<IWICBitmap> bitmap;
 		hr = factory->CreateBitmapFromSource(frame, WICBitmapCacheOnDemand, &bitmap);
-		if(FAILED(hr))
+		if (FAILED(hr))
 		{
 			std::wcout << L"failed: CreateBitmapFromSource\n";
 			return false;
 		}
 
 		CComPtr<IWICBitmapLock> lock;
-		WICRect rcLock{ 0, 0, (INT)width, (INT)height };
+		WICRect rcLock{0, 0, (INT)width, (INT)height};
 		hr = bitmap->Lock(&rcLock, WICBitmapLockRead, &lock);
-		if(FAILED(hr))
+		if (FAILED(hr))
 		{
 			std::wcout << L"failed: IWICBitmap::Lock\n";
 			return false;
@@ -150,50 +235,67 @@ bool load(IWICImagingFactory* factory, CComPtr<IWICBitmapDecoder> decoder)
 		UINT stride;
 		uint8_t *pv = nullptr;
 		hr = lock->GetStride(&stride);
-		if(FAILED(hr))
+		if (FAILED(hr))
 		{
 			std::wcout << L"failed: IWICBitmapLock::GetStride\n";
 			return false;
 		}
 
 		hr = lock->GetDataPointer(&buffersize, &pv);
-		if(FAILED(hr))
+		if (FAILED(hr))
 		{
 			std::wcout << L"failed: IWICBitmapLock::GetDataPointer\n";
 			return false;
 		}
-
+		CComPtr<IWICColorContext> colorContext;
+		hr = factory->CreateColorContext(&colorContext);
+		if (FAILED(hr))
+		{
+			std::wcout << L"failed: CreateColorContext\n";
+			return false;
+		}
+		UINT ccCount = 0;
+		// need IWICColorContext ptr
+		hr = frame->GetColorContexts(1, &(colorContext.p), &ccCount);
+		if (SUCCEEDED(hr) && ccCount == 1)
+		{
+			UINT profileLength = 0;
+			hr = colorContext->GetProfileBytes(0, nullptr, &profileLength);
+			if (SUCCEEDED(hr)) // heicで何故か失敗するのでエラー扱いにしない
+			{
+				auto profile = std::make_unique<uint8_t[]>(profileLength);
+				hr = colorContext->GetProfileBytes(profileLength, profile.get(), &profileLength);
+				if (SUCCEEDED(hr))
+					parseICCProfile(profile.get(), profileLength);
+			}
+		}
 	}
 	return true;
 }
 
-int wmain(int argc, wchar_t* argv[])
+int wmain(int argc, wchar_t *argv[])
 {
 	std::wcout.imbue(std::locale(""));
-	if(argc <= 1)
+	if (argc <= 1)
 	{
-		std::wcout << L"Syntax:  wicc [options or files ...]" << std::endl << std::endl;
+		std::wcout << L"Syntax:  wicc [options or files ...]" << std::endl
+				   << std::endl;
 		std::wcout << L"Options: begins with [- or /]" << std::endl;
 		std::wcout << L" l, L    List installed Components" << std::endl;
-		std::wcout << L" c, C    Show Componentinfo" << std::endl;
+		std::wcout << L" i, I    Show Componentinfo" << std::endl;
 		std::wcout << L" m, M    Preferred Microsoft Component" << std::endl;
 		std::wcout << L" oldpng  Use Windows7 PNG Decoder" << std::endl;
 		return 0;
 	}
 
-	HRESULT hr = ::CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
-	if(FAILED(hr))
-	{
-		std::wcout << L"failed: CoInitializeEx\n";
-		goto exit;
-	}
-
-	IWICImagingFactory* factory = nullptr;
-	hr = ::CoCreateInstance(CLSID_WICImagingFactory, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&factory));
-	if(FAILED(hr))
+	COMContext comCtx;
+	CComPtr<IWICImagingFactory> factory;
+	HRESULT hr = ::CoCreateInstance(CLSID_WICImagingFactory, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&factory));
+	if (FAILED(hr))
 	{
 		std::wcout << L"failed: CoCreateInstance(IWICImagingFactory)\n";
-		goto exit;
+		std::cout << "hr:" << hresult_tostr(hr) << "\n";
+		return hr;
 	}
 
 	LARGE_INTEGER freq, before, after;
@@ -202,78 +304,77 @@ int wmain(int argc, wchar_t* argv[])
 
 	bool force_oldpng = false;
 	bool show_componentinfo = false;
-	const GUID* preferred_vender = nullptr;
-	for(size_t i = 1; i < argc; i++)
+	const GUID *preferred_vender = nullptr;
+	for (int i = 1; i < argc; i++)
 	{
 		auto arg = argv[i];
-		if(arg[0] == L'-'|| arg[0] == L'/')
+		if (arg[0] == L'-' || arg[0] == L'/')
 		{
-			if(_wcsicmp(arg + 1, L"oldpng") == 0)
+			if (_wcsicmp(arg + 1, L"oldpng") == 0)
 				force_oldpng = true;
-			else if(arg[1] == L'l' || arg[1] == L'L')
+			else if (arg[1] == L'l' || arg[1] == L'L')
 				enumComponets(factory);
-			else if(arg[1] == L'c' || arg[1] == L'C')
+			else if (arg[1] == L'i' || arg[1] == L'I')
 				show_componentinfo = true;
-			else if(arg[1] == L'm' || arg[1] == L'M')
+			else if (arg[1] == L'm' || arg[1] == L'M')
 				preferred_vender = &GUID_VendorMicrosoft;
-		}else
+		}
+		else
 		{
 			CComPtr<IWICBitmapDecoder> decoder;
 			CComPtr<IWICStream> stream;
 			std::wcout << arg << std::endl;
-			if(force_oldpng)
+			if (force_oldpng)
 			{
 				hr = factory->CreateDecoder(CLSID_WICPngDecoder1, nullptr, &decoder);
 				hr = decoder.CoCreateInstance(CLSID_WICPngDecoder1);
-				if(FAILED(hr))
+				if (FAILED(hr))
 				{
 					std::wcout << L"failed: CreateDecoder(CLSID_WICPngDecoder1)\n";
-					goto exit;
+					break;
 				}
 
 				hr = factory->CreateStream(&stream);
-				if(FAILED(hr))
+				if (FAILED(hr))
 				{
 					std::wcout << L"failed: CreateStream\n";
-					goto exit;
+					break;
 				}
 				hr = stream->InitializeFromFilename(arg, GENERIC_READ);
-				if(FAILED(hr))
+				if (FAILED(hr))
 				{
 					std::wcout << L"failed: IWICStream::InitializeFromFilename\n";
-					goto exit;
+					break;
 				}
 				decoder->Initialize(stream, WICDecodeMetadataCacheOnDemand);
-				if(FAILED(hr))
+				if (FAILED(hr))
 				{
 					std::wcout << L"failed: IWICBitmapDecoder::Initialize\n";
-					goto exit;
-				}
-			}else
-			{
-				hr = factory->CreateDecoderFromFilename(arg, preferred_vender, GENERIC_READ, WICDecodeMetadataCacheOnDemand, &decoder);
-				if(FAILED(hr))
-				{
-					std::wcout << L"failed: CreateDecoderFromFilename\n";
-					goto exit;
+					break;
 				}
 			}
-			if(show_componentinfo)
+			else
+			{
+				hr = factory->CreateDecoderFromFilename(arg, preferred_vender, GENERIC_READ, WICDecodeMetadataCacheOnDemand, &decoder);
+				if (FAILED(hr))
+				{
+					std::wcout << L"failed: CreateDecoderFromFilename\n";
+					break;
+				}
+			}
+			if (show_componentinfo)
 				decoderinfo(decoder);
 			load(factory, decoder);
 			std::wcout << std::endl;
 		}
 	}
-exit:
+
 	QueryPerformanceCounter(&after);
-	if(freq.QuadPart != 0)
+	if (freq.QuadPart != 0)
 	{
 		double time = (double)(after.QuadPart - before.QuadPart) / (double)freq.QuadPart;
 		std::wcout << L"TotalTime: " << std::fixed << time;
 	}
-	if(factory)
-		factory->Release();
-	CoUninitialize();
+
 	return 0;
 }
-
